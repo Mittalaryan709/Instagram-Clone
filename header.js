76ce9229c33503739bb8136bb0db0897e480d5e0
Mittalaryan709 <120815291+Mittalaryan709@users.noreply.github.com>
2024-10-04 22:22:57 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header style={{ padding: '20px', backgroundColor: '#ffffff', borderBottom: '1px solid #eaeaea' }}>
+      <h1 style={{ margin: 0 }}>Figma Clone</h1>
+    </header>
+  );
+};
+
+export default Header;
